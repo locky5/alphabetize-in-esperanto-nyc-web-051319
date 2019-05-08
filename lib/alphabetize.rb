88f699dd1ee2437ecb544ac65ef30022deec834f
Kevin Dai <daikevin19@yahoo.com>
2019-05-08 07:07:21 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
   arr.sort_by do |word| # alphabetizes the array 
-    word.split('').collect do |letter|
+    word.split('').collect do |letter| # 
       ESPERANTO_ALPHABET.index(letter)  
     end
   end
