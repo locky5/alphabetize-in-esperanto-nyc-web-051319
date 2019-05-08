5cda5f2cfd878d4a9258ee05bb40181098ee8a5f
Kevin Dai <daikevin19@yahoo.com>
2019-05-08 07:09:28 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,9 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by do |word| # alphabetizes the array 
-    word.split('').collect do |letter| # 
-      ESPERANTO_ALPHABET.index(letter)  
+  arr.sort_by do |word| # alphabetizes the array then enumerates
+    word.split('').collect do |letter| # split words then letter by letter
+      ESPERANTO_ALPHABET.index(letter) # 
     end
   end
 end
